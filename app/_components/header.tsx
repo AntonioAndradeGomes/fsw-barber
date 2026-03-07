44558603d242e6c262b725d8b38e0f7b0a1a003a
AntonioAndradeGomes <gomesmax1997@gmail.com>
2026-03-07 20:48:05 -0300

    feat: add sidebar

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,7 +1,25 @@
-import { CalendarDays, CircleUserIcon, MenuIcon } from "lucide-react"
+import {
+    CalendarDays,
+    CalendarIcon,
+    CircleUserIcon,
+    HomeIcon,
+    LogOutIcon,
+    MenuIcon,
+} from "lucide-react"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "./ui/sheet"
+import quickSearchOptions from "../_constants/search"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import Link from "next/link"
 
 const Header = () => {
     return (
@@ -15,9 +33,84 @@ const Header = () => {
                         width={120}
                     />
                     <div className="block sm:hidden">
-                        <Button size="icon" variant="outline">
-                            <MenuIcon />
-                        </Button>
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button size="icon" variant="outline">
+                                    <MenuIcon />
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent>
+                                <SheetHeader>
+                                    <SheetTitle className="text-left">
+                                        Menu
+                                    </SheetTitle>
+                                </SheetHeader>
+
+                                <div className="flex items-center gap-3 border-b border-solid py-5">
+                                    <Avatar>
+                                        <AvatarImage src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                                    </Avatar>
+                                    <div>
+                                        <p className="font-bold">
+                                            Antonio Andrade
+                                        </p>
+                                        <p className="text-xs">
+                                            gomesmax1997@gmail.com
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col gap-1 border-b border-solid py-5">
+                                    <SheetClose asChild>
+                                        <Button
+                                            className="justify-start gap-2"
+                                            variant="ghost"
+                                            asChild
+                                        >
+                                            <Link href={"/"}>
+                                                <HomeIcon size={18} />
+                                                Início
+                                            </Link>
+                                        </Button>
+                                    </SheetClose>
+
+                                    <Button
+                                        className="justify-start gap-2"
+                                        variant="ghost"
+                                    >
+                                        <CalendarIcon size={18} />
+                                        Agendamentos
+                                    </Button>
+                                </div>
+
+                                <div className="flex flex-col gap-1 border-b border-solid py-5">
+                                    {quickSearchOptions.map((item) => (
+                                        <Button
+                                            className="justify-start gap-2"
+                                            variant="ghost"
+                                            key={item.title}
+                                        >
+                                            <Image
+                                                src={item.imageUrl}
+                                                height={18}
+                                                width={18}
+                                                alt={item.title}
+                                            />
+                                            {item.title}
+                                        </Button>
+                                    ))}
+                                </div>
+
+                                <div className="flex flex-col gap-1 py-5">
+                                    <Button
+                                        className="justify-start gap-2"
+                                        variant="ghost"
+                                    >
+                                        <LogOutIcon size={18} />
+                                        Sair da conta
+                                    </Button>
+                                </div>
+                            </SheetContent>
+                        </Sheet>
                     </div>
 
                     <div className="hidden sm:block">
