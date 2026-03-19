f95a97558a507b0c6a6530abc165198c1b6ad64c
AntonioAndradeGomes <gomesmax1997@gmail.com>
2026-03-19 15:29:49 -0300

    feat: Adding the login to the header when in full screen mode.

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,26 @@
-import { CalendarDays, CircleUserIcon, MenuIcon } from "lucide-react"
+"use client"
+import { CalendarDays, MenuIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
 import Link from "next/link"
+import { signIn, useSession } from "next-auth/react"
+import { Avatar, AvatarImage } from "./ui/avatar"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "./ui/dialog"
 
 const Header = () => {
+    const { data } = useSession()
+    const handleLoginWithGoogleClick = () =>
+        signIn("google", { prompt: "select_account" })
     return (
         <header>
             <Card>
@@ -36,10 +50,58 @@ const Header = () => {
                             <Button className="gap-2" variant="ghost">
                                 <CalendarDays /> Agendamento
                             </Button>
-                            <Button className="gap-2">
-                                <CircleUserIcon />
-                                Perfil
-                            </Button>
+                            {data?.user ? (
+                                <div className="flex items-center gap-2">
+                                    <Avatar className="h-8 w-8">
+                                        <AvatarImage
+                                            src={data?.user?.image ?? ""}
+                                        />
+                                    </Avatar>
+
+                                    <p className="font-bold">
+                                        {data.user.name}
+                                    </p>
+                                </div>
+                            ) : (
+                                <Dialog>
+                                    <DialogTrigger asChild>
+                                        <Button className="gap-2">
+                                            <Image
+                                                src="/google.svg"
+                                                width={18}
+                                                height={18}
+                                                alt="Login com o Google"
+                                            />
+                                            Login com o Google
+                                        </Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="w-[90%]">
+                                        <DialogHeader>
+                                            <DialogTitle>
+                                                Faça login na plataforma.
+                                            </DialogTitle>
+                                            <DialogDescription>
+                                                Conecte-se usando sua conta do
+                                                Google.
+                                            </DialogDescription>
+                                        </DialogHeader>
+
+                                        <Button
+                                            variant="outline"
+                                            className="gap-1 font-bold"
+                                            onClick={handleLoginWithGoogleClick}
+                                        >
+                                            <Image
+                                                src="/google.svg"
+                                                width={18}
+                                                height={18}
+                                                alt="Login com o Google"
+                                            />
+                                            Login com o Google
+                                        </Button>
+                                    </DialogContent>
+                                </Dialog>
+                            )}
                         </nav>
                     </div>
                 </CardContent>
