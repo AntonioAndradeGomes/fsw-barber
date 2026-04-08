f27094e5c320946ea11c3545690c403bc9a1a48b
AntonioAndradeGomes <gomesmax1997@gmail.com>
2026-04-08 14:58:21 -0300

    feat: ensure bookings are listed on bookings page

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,38 @@ import BookingItem from "./_components/booking-item"
 import BarberShopList from "./_components/barbershop-list"
 import Search from "./_components/search"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
 
 const Home = async () => {
+    const session = await getServerSession(authOptions)
     const barbershops = await db.barbershop.findMany()
     const popularBarberShops = await db.barbershop.findMany({
         orderBy: {
             name: "desc",
         },
     })
+
+    const confirmedBookings = session?.user
+        ? await db.booking.findMany({
+              where: {
+                  userId: session?.user?.id,
+                  date: {
+                      gte: new Date(),
+                  },
+              },
+              include: {
+                  service: {
+                      include: {
+                          barberShop: true,
+                      },
+                  },
+              },
+              orderBy: {
+                  date: "asc",
+              },
+          })
+        : []
 
     return (
         <div>
@@ -116,7 +140,14 @@ const Home = async () => {
                     />
                 </div>
 
-                <BookingItem />
+                <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
+                    Agendamentos
+                </h2>
+                <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+                    {confirmedBookings.map((booking) => (
+                        <BookingItem key={booking.id} booking={booking} />
+                    ))}
+                </div>
             </div>
             <div className="px-5 pb-5">
                 <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
